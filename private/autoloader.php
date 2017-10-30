1544714f8d410becc3484df5098ecab0ba7794f6
Agustin Ortu <ortu.agustin@gmail.com>
2017-10-30 13:51:31 -0300

    muevo el codigo que habilita los errores al index.php

--- private/autoloader.php
+++ private/autoloader.php
@@ -1,8 +1,4 @@
 <?php
-ini_set('display_startup_errors', 1);
-ini_set('display_errors', 1);
-error_reporting(-1);
-
 require_once './Slim/autoloader.php';
 require_once 'private/model/PDORepository.php';
 
