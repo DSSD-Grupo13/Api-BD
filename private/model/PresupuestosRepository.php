bcdf94caee6a3b2a4826120c4d97f612e70cbb6e
Ortu Agustin <ortu.agustin@gmail.com>
2017-11-20 16:36:22 -0300

    metodo POST /presupuestos/{id_incidente}: faltaba insertar en la tabla presupuestos_incidente

--- private/model/PresupuestosRepository.php
+++ private/model/PresupuestosRepository.php
@@ -2,23 +2,31 @@
 class PresupuestosRepository extends PDORepository
 {
   private $stmtCreatePresupuesto;
+  private $stmtLinkPresupuestoIncidente;
   private $stmtCreateObjeto;
+  private $qryFetchIdPresupuesto;
 
   public function __construct()
   {
+    $this->qryFetchIdPresupuesto = $this->newPreparedStmt("SELECT idPresupuesto FROM presupuesto ORDER BY idPresupuesto DESC LIMIT 1");
     $this->stmtCreatePresupuesto = $this->newPreparedStmt("INSERT INTO presupuesto (total) VALUES (?)");
-    $this->stmtCreateObjeto = $this->newPreparedStmt("INSERT INTO objetos_presupuesto
-                    (idPresupuesto, nombre, cantidad, descripcion, precio, total) VALUES (?, ?, ?, ?, ?, ?)");
+    $this->stmtLinkPresupuestoIncidente = $this->newPreparedStmt("INSERT INTO presupuestos_incidente (idIncidente, idPresupuesto) VALUES (?, ?)");
+    $this->stmtCreateObjeto = $this->newPreparedStmt("INSERT INTO objetos_presupuesto (idPresupuesto, nombre, cantidad, descripcion, precio, total) VALUES (?, ?, ?, ?, ?, ?)");
   }
 
   public function create($idIncidente, $objetos, $total_final)
   {
     $this->stmtCreatePresupuesto->execute([$total_final]);
-    $qry = $this->newPreparedStmt("SELECT idPresupuesto FROM presupuesto ORDER BY idPresupuesto DESC LIMIT 1");
-    $qry->execute();
-    $idPresupuesto = $qry->fetchColumn();
+    $idPresupuesto = $this->fetchIdPresupuesto();
+    $this->stmtLinkPresupuestoIncidente->execute([$idIncidente, $idPresupuesto]);
     $this->createObjects($idPresupuesto, $objetos);
     return $idPresupuesto;
+  }
+
+  private function fetchIdPresupuesto()
+  {
+    $this->qryFetchIdPresupuesto->execute();
+    return $this->qryFetchIdPresupuesto->fetchColumn();
   }
 
   private function createObjects($idPresupuesto, $objetos)
